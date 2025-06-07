9ad24e2167937abe1e6d2e0f3a22118523907eea
EmanuelFrigodeOliveira <5196945@alunouninter.com>
2025-06-07 20:31:17 -0300

    hi

--- namingAlgorithm/namingScript.py
+++ namingAlgorithm/namingScript.py
@@ -6,7 +6,7 @@ my_files = glob.glob(os.path.join(directory, '*jpg'))
 my_files.sort(key=os.path.getctime)
 
 for index, file_path in enumerate(my_files):
-    new_name = f"Foto_{str(index + 1).zfill(2)}.jpg"
+    new_name = f"Foto_{str(index + 1)}.jpg"
     new_file_path = os.path.join(directory, new_name)
 
     os.rename(file_path, new_file_path)
